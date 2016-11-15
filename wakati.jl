6e989f07a992099025a776ca543a5394f6871e5a
kwnsiy <sei5512kkk@yahoo.co.jp>
2016-11-15 13:00:09 +0900

    Update wakati.jl

--- wakati.jl
+++ wakati.jl
@@ -7,7 +7,9 @@
 using MeCab
 using TinySegmenter
 
+string = "すもももももももものうち"
+
 mecab = Mecab()
-results = parse(mecab, "すもももももももものうち")
+results = parse(mecab, string)
 println(join([result.surface for result in results], "|"))
 println(join(tokenize(string), "|"))
