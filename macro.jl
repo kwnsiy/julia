5b73bc9d1191125f90c6a7b98ef127e68c4835c3
kwnsiy <sei5512kkk@yahoo.co.jp>
2016-11-30 17:03:17 +0900

    Create macro.jl

---
+++ macro.jl
@@ -0,0 +1,71 @@
+
+""" @swap! """
+macro swap!(x,y)
+  quote
+    $(esc(x)) = $(eval(y))
+    $(esc(y)) = $(eval(x))
+    return
+  end
+end
+
+
+""" @cd """
+macro cd(obj)
+  :(cd($obj))
+end
+
+
+""" @pwd """
+macro pwd()
+  :(pwd())
+end
+
+""" @replace! """
+macro replace!(string, pat, r)
+  quote
+    $(esc(string)) = $(eval(replace(eval(string),eval(pat), eval(r))))
+    return
+  end
+end
+
+
+""" @split! """
+macro split!(string, chars)
+  quote
+    $(esc(string)) = $(eval(split(eval(string),eval(chars))))
+    return
+  end
+end
+
+
+""" @rstrip! """
+macro rstrip!(string)
+  quote
+    $(esc(string)) = $(eval(rsrtrip(eval(string))))
+    return
+  end
+end
+
+
+""" @println """
+macro println(obj)
+  :(println($obj))
+end
+
+
+""" @print """
+macro print(obj)
+  :(print($obj))
+end
+
+
+""" @inc! """
+macro inc!(x)
+   :($x += 1)
+end
+
+
+""" @decr! """
+macro decr!(x)
+  :($x -= 1)
+end
