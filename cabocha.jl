b4de76affc98a7cbdac6582955a897e7f59b4aae
kwnsiy <sei5512kkk@yahoo.co.jp>
2016-12-04 20:56:20 +0900

    Update cabocha.jl

--- cabocha.jl
+++ cabocha.jl
@@ -1,17 +1,12 @@
 # coding:utf-8
 
-using XMLconvert
-
 doc = 
 """
 usage: 
   cabocha_parser("クロールで泳いでいる少女を見た")
-  This function takes a LightXML.XMLDocument as a return value.
 """
 
 """ cabocha_parser """
-function cabocha_parser(sentence; xml = true)
-  xml == true && return parse_string(readall(pipeline(`echo $sentence`, `cabocha -f3`)))
-  xml != true && return readall(pipeline(`echo $sentence`, `cabocha -f1`))
+function cabocha_parser(sentence; option = "f1")
+  return readall(pipeline(`echo $sentence`, `cabocha -$option`))
 end
-
